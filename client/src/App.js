72a9411fae84ddf3a2684740805805edb9d7420e
Ernie Rimer <ernest.rimer@icloud.com>
2020-06-29 20:16:22 -0600

    initial

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+import Home from './components/shared/Home';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Navbar from './components/shared/Navbar';
+import NoMatch from './components/shared/NoMatch';
+const App = () => (
+  <>
+    <Navbar />
+    <Container>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
+        <Route component={NoMatch} />
+      </Switch>
+    </Container>
+  </>
+)
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'semantic-ui-css/semantic.min.css';
+import { BrowserRouter } from 'react-router-dom';
+import AuthProvider from './providers/AuthProvider';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
